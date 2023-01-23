0e49e86df9f9c03ea4da41b43db1cdfd39e759a2
denis-ttk-1975 <denis.ttk@gmail.com>
2023-01-23 18:25:07 +0300

    mongoose connected

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,13 @@
-const _u = 0;
+import express from "express";
+import mongoose from "mongoose";
 
-console.log(_u);
+// Слушаем 3000 порт
+const { PORT = 3000 } = process.env;
+
+const app = express();
+mongoose.connect("mongodb://localhost:27017/mestodb");
+
+app.listen(PORT, () => {
+  // Если всё работает, консоль покажет, какой порт приложение слушает
+  console.log(`App listening on port ${PORT}`);
+});
