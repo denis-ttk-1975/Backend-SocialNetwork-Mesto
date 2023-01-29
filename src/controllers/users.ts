50feffc691764b6fa707d150b4615ce0ddc71efb
denis-ttk-1975 <denis.ttk@gmail.com>
2023-01-29 14:46:01 +0300

    errorHandler build in users controller

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -3,26 +3,27 @@ import { ObjectId } from "mongodb";
 
 import User from "./../models/users";
 import { IRequest } from "./../app";
+import errorHandler from "./../utils";
 
 export const createUser = (req: Request, res: Response) => {
   const { name, about, avatar } = req.body;
-
+  console.log("createUser");
   return User.create({ name, about, avatar })
     .then((user) => res.send({ data: user }))
-    .catch(() => res.status(500).send({ message: "Произошла ошибка" }));
+    .catch((err) => errorHandler(err, res));
 };
 
 export const getUsers = (req: Request, res: Response) => {
   return User.find({})
     .then((users) => res.send({ data: users }))
-    .catch(() => res.status(500).send({ message: "Произошла ошибка" }));
+    .catch((err) => errorHandler(err, res));
 };
 
 export const getUser = (req: Request, res: Response) => {
   const { _id } = req.params;
   return User.find({ _id: new ObjectId(_id) })
     .then((user) => res.send({ data: user }))
-    .catch(() => res.status(500).send({ message: "Произошла ошибка" }));
+    .catch((err) => errorHandler(err, res));
 };
 
 export const patchUserData = (req: IRequest, res: Response) => {
@@ -31,7 +32,7 @@ export const patchUserData = (req: IRequest, res: Response) => {
 
   return User.findByIdAndUpdate(userId, { name, about })
     .then((user) => res.send({ data: user }))
-    .catch((err) => res.status(500).send({ message: "Произошла ошибка" }));
+    .catch((err) => errorHandler(err, res));
 };
 
 export const patchUserAvatar = (req: IRequest, res: Response) => {
@@ -40,5 +41,5 @@ export const patchUserAvatar = (req: IRequest, res: Response) => {
 
   return User.findByIdAndUpdate(userId, { avatar })
     .then((user) => res.send({ data: user }))
-    .catch((err) => res.status(500).send({ message: "Произошла ошибка" }));
+    .catch((err) => errorHandler(err, res));
 };
